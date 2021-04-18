604466f434c72d58f4533a54a9db70467bb350d2
datakitsch9761 <data.kitsch@gmail.com>
2021-04-18 23:31:28 +0900

    javaTpoint self study material - day02 Static Variable

--- src/java_basic_programs/HelloWorld.java
+++ src/java_basic_programs/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World What is going on");
         System.out.println("I am trying to fix my git hub");
+        //
     }
 }
