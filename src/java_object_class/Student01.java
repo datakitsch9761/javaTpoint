1296acea7af03ab6ffb3a50eb8648311ae7a1920
datakitsch9761 <josephhwanlee9761@gmail.com>
2021-04-18 19:01:26 +0900

    javaTpoint self study material - day02 Constructor and Method

--- src/java_object_class/Student01.java
+++ src/java_object_class/Student01.java
@@ -2,15 +2,15 @@ package java_object_class;
 // Java program to illustrate how define a class and fields
 // Defining a student class.
 
-class Student {
+class Student01 {
 //defining fields
-int id;//field or data member or instance variables
+int id = 0;//field or data member or instance variables
 String name;
 // creating main method inside the Student class
 
 public static void main(String[] args) {
     //creating an object or instance
-    Student s1 = new Student();
+    Student01 s1 = new Student01();
     System.out.println(s1.id);
     System.out.println(s1.name);
     }
