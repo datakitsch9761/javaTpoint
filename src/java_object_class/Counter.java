448af0df15b86c0dbb3557dd8b832a69881e29fe
datakitsch9761 <josephhwanlee9761@gmail.com>
2021-04-18 23:03:15 +0900

    javaTpoint self study material - day02 Static Variable

--- src/java_object_class/Counter.java
+++ src/java_object_class/Counter.java
@@ -2,6 +2,7 @@ package java_object_class;
 //Java program to demonstrate the use of an instance variable
 //Which get memory each time when we create an object of the class
 //
+//
 class Counter {
     int count = 0; //will get memory each time when the instance is created
 
