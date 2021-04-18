ce89f09e7d5e70a5a20dc1db2d5b18c475439c96
datakitsch9761 <josephhwanlee9761@gmail.com>
2021-04-18 21:40:38 +0900

    javaTpoint self study material - day02 Static

---
+++ src/java_object_class/TestStaticVariable1.java
@@ -0,0 +1,31 @@
+package java_object_class;
+
+public class TestStaticVariable1{
+    public static void main(String[] args) {
+        Student11 s1 = new Student11(111,"Karan");
+        Student11 s2 = new Student11(222,"Aryan");
+        //we can change the college of all objects by the single line of code
+        //Student.college = "BBDIT";
+        s1.display();
+        s2.display();
+    }
+}
+
+class Student11 {
+    //instance variable
+    int rollno;
+    String name;
+    static String college = "ITS";//Static Variable
+
+    //constructor
+    Student11(int r, String n) {
+        rollno = r;
+        name = n;
+    }
+
+    //method to display the values
+    void display() {
+        System.out.println(rollno + " " + name + " " + college);
+    }
+
+}
