a5c0651f8e2082c39f6f1b69f553c75fe02feba9
datakitsch9761 <data.kitsch@gmail.com>
2021-04-28 15:14:09 +0900

    test commit

---
+++ src/java_arrays/example.java
@@ -0,0 +1,8 @@
+package java_arrays;
+
+public class example {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hellow this is Joseph");
+    }
+}
